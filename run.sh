b82646d00d6217ea87ba98ee01f0c5f77819bd85
arturozv <zzarturozz@gmail.com>
2017-05-26 12:27:21 -0700

    gc params

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 ./mvnw clean test install assembly:single
-java -jar ./target/digit-processor-jar-with-dependencies.jar
+java -server -XX:+UseG1GC -Xms4g -XX:MaxGCPauseMillis=100 -XX:ParallelGCThreads=4 -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:gc.log \
+-jar ./target/digit-processor-jar-with-dependencies.jar
+
+
+#-server -XX:+UseG1GC -Xms4g -XX:MaxGCPauseMillis=100 -XX:ParallelGCThreads=4 -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:gc.log
+#-server -XX:+UseG1GC -Xms4g -XX:MaxGCPauseMillis=100 -XX:+UseParallelGC  -XX:ParallelGCThreads=4 -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:gc.log
